5de59d599196705c6da6bf32e86cbb71926281cc
murphy-afk <bardarenoemi@gmail.com>
2026-07-10 11:56:35 +0200

    changes for deploy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->append(CustomCors::class);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
